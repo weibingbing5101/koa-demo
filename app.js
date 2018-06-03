05176c5452917ce7b7e594561d81a47a80cd1b1c
魏冰-mac-13 <510137831@qq.com>
2018-06-03 13:23:25 +0800

    mysql

--- app.js
+++ app.js
@@ -1,39 +1,274 @@
 const Koa = require('koa')
-// 注意 require('koa-router') 返回的是函数:
 const router = require('koa-router')();
-const bodyParser = require('koa-bodyparser')
+const bodyparse = require('koa-bodyparser');  // 格式化 post请求的body数据
+
+
+// const MysqlStore = require('koa-mysql-session');
+// const session = require('koa-session-minimal');
+// 使用koa-session-minimal   koa-mysql-session来进行数据库的操作
+
+
+const app = new Koa();
 
 
 
-const app = new Koa()
-app.use(bodyParser())
 
 
 
 
-// 添加路由
-router.get(
-    '/users/:id',
-    async(ctx, next) => {
-        console.log(ctx);
-        ctx.body = { name: 111, age: 22222 };
 
-        // next();
-        // await User.findOne(ctx.params.id).then(function(user) {
-        //     // 首先读取用户的信息，异步操作
-        //     ctx.user = user;
-        //     next();
-        // });
-    }
-);
 
-// 调用路由中间件
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// session存储配置
+// const sessionMysqlConfig= {
+//   user: config.database.USERNAME,
+//   password: config.database.PASSWORD,
+//   database: config.database.DATABASE,
+//   host: config.database.HOST,
+// }
+
+// 配置session中间件
+// app.use(session({
+//   key: 'USER_SID',
+//   store: new MysqlStore(sessionMysqlConfig)
+// }))
+
+
+
+
+
+
+app.use(bodyparse());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.use(require('./routers/signin.js').routes())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.use(router.routes())
 
 app.listen(3000, () => {
     console.log('server is running at http://localhost:3000')
 })
 
-app.on('error', (err, ctx) => {
-    log.error('server error', err, ctx)
+// app.on('error', (err, ctx) => {
+//     log.error('server error', err, ctx)
+// });
+
+
+//  执行顺序说明
+/*
+app.use(async(ctx, next)=>{
+    console.log(111);
+    await next();
+    ctx.response.type="text/html";
+    ctx.response.body = '<div>hello world111</div>';
+    console.log(11100000);
+
 });
+
+
+app.use(async(ctx, next)=>{
+    console.log(222);
+
+    await next();
+    ctx.response.type="text/html";
+    ctx.response.body = '<div>hello world222</div>';
+    console.log(2220000);
+
+    
+})
+
+
+app.use(async(ctx, next)=>{
+    console.log(333);
+    ctx.response.type="text/html";
+    ctx.response.body = '<div>hello world333</div>';
+    console.log(3330000);
+})
+
+111
+222
+333
+3330000
+2220000
+11100000
+*/
+
+
+
+
+// router.get('/api/getSliderImg', async(ctx, next)=>{
+//     let data = await fs.readFileSync('./getSliderImg.js','utf-8');
+//     ctx.response.type="application/json";
+//     ctx.response.body = data;
+// })
+
+// router.get('/api/getHotNews', async(ctx, next)=>{
+//     let data = await fs.readFileSync('./getHotNews.js','utf-8');
+//     ctx.response.type="application/json";
+//     ctx.response.body = data;
+// })
+
+// router.get('/api/getSaveNews', async(ctx, next)=>{
+//     let data = await fs.readFileSync('./getSaveNews.js','utf-8');
+//     ctx.response.type="application/json";
+//     ctx.response.body = data;
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ config/default.js
@@ -0,0 +1,15 @@
+// 数据库链接的配置文件
+const config = {
+  // 启动端口
+  port: 3000,
+  // 数据库配置
+  database: {
+    DATABASE: 'nodesql',
+    USERNAME: 'root',
+    PASSWORD: '12345678',
+    PORT: '3306',
+    HOST: 'localhost'
+  }
+}
+
+module.exports = config
